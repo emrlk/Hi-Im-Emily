7c6dfa49ca9178c016ff0fe94713d4c8500d54a3
Emily Louk <emilyrl222@gmail.com>
2024-04-30 00:12:45 -0400

    Fix typo

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
     >
       <div className="w-full min-h-[800px] h-screen xl:w-[70%] relative mt-40 flex flex-col items-center justify-center pb-36">
         <div className="w-full h-[180%] mt-[-10%] mb-[-10%] relative z-10">
-          <SectionTitle title="PROJECTS" subtitle="Personal & Acedemic Projects" />
+          <SectionTitle title="PROJECTS" subtitle="Personal & Academic Projects" />
         </div>
 
         <div
